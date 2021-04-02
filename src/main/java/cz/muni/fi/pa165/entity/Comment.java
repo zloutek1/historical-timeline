f6fc587b86744e2487d817e94ead69aeb7ca4531
Ondrej Machala <ondra097@seznam.cz>
2021-04-02 17:13:57 +0200

    Add Comment entity

--- src/main/java/cz/muni/fi/pa165/entity/Comment.java
+++ src/main/java/cz/muni/fi/pa165/entity/Comment.java
@@ -1,4 +1,57 @@
 package cz.muni.fi.pa165.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.sql.Time;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+/**
+ * Class represents a comment under a timeline.
+ *
+ * @author Ondřej Machala
+ */
+@Entity
+@Getter
+@ToString
 public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter
+    private Long id;
+
+    @NotNull
+    @Setter
+    private String text;
+
+    @NotNull
+    @Setter
+    private LocalDateTime time;
+
+    @ManyToOne(optional = false)
+    @NotNull
+    @Setter
+    private Timeline timeline;
+
+    @ManyToOne(optional = false)
+    @NotNull
+    @Setter
+    private User author;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Comment)) return false;
+        Comment comment = (Comment) o;
+        return Objects.equals(getText(), comment.getText()) && Objects.equals(getTime(), comment.getTime());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getText(), getTime());
+    }
 }

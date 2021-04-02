b5ca1f79a66922230fe6ca5f173017da3d31af5c
Ondrej Machala <ondra097@seznam.cz>
2021-04-02 13:03:29 +0200

    Add StudyGroup entity

--- src/main/java/cz/muni/fi/pa165/entity/StudyGroup.java
+++ src/main/java/cz/muni/fi/pa165/entity/StudyGroup.java
@@ -1,4 +1,50 @@
 package cz.muni.fi.pa165.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * Class representing a study group for {@link User users}.
+ *
+ * @author Ondřej Machala
+ */
+@Entity
+@Getter
+@ToString
 public class StudyGroup {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter
+    private Long id;
+
+    @NotNull
+    @Column(nullable = false, unique = true)
+    @Setter
+    private String name;
+
+    @ManyToMany
+    private List<User> users = new ArrayList<>();
+
+    @OneToMany(mappedBy = "studyGroup")
+    private List<Timeline> timelines = new ArrayList<>();
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof StudyGroup)) return false;
+        StudyGroup that = (StudyGroup) o;
+        return Objects.equals(getName(), that.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName());
+    }
 }

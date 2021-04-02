cbb07e0dd2698260fb4f01f7df5b73666928a548
Tomáš Ljutenko <485503@mail.muni.cz>
2021-04-02 11:17:19 +0200

    implemented Timeline entity

--- src/main/java/cz/muni/fi/pa165/entity/Timeline.java
+++ src/main/java/cz/muni/fi/pa165/entity/Timeline.java
@@ -1,5 +1,55 @@
 package cz.muni.fi.pa165.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+import java.util.Objects;
+
+/**
+ * Class representing a historical timeline of {@link Event events}.
+ *
+ * @author Tomáš Ljutenko
+ */
+@Entity
+@Getter
+@Setter
+@ToString
 public class Timeline {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String name;
+
+    private LocalDate from;
+
+    private LocalDate to;
+
+    //@OneToMany
+    //private List<Comment> comments = new ArrayList<>();
+
+    //@OneToMany
+    //private Set<Event> events = new HashSet<>();
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Timeline)) return false;
+        Timeline timeline = (Timeline) o;
+        return getName().equals(timeline.getName()) && Objects.equals(getFrom(), timeline.getFrom()) && Objects.equals(getTo(), timeline.getTo());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), getFrom(), getTo());
+    }
 }

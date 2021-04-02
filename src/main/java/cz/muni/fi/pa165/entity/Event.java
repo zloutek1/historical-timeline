488ac538b30f60ec24d9cc681e56377eda3f8730
Eva Krajníková <xkrajnik@fi.muni.cz>
2021-04-02 15:50:40 +0200

    Timelines changed to ManyToMany

--- src/main/java/cz/muni/fi/pa165/entity/Event.java
+++ src/main/java/cz/muni/fi/pa165/entity/Event.java
@@ -1,4 +1,62 @@
 package cz.muni.fi.pa165.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * Class representing a historical event.
+ *
+ * @author Eva Krajíková
+ */
+@Entity
+@Getter
+@ToString
 public class Event {
+    @Id
+    @Setter
+    private int id;
+
+    @NotNull
+    @Column(nullable = false, unique = true)
+    @Setter
+    private String name;
+
+    @Setter
+    private LocalDate date;
+
+    @Setter
+    private String location;
+
+    @Setter
+    private String description;
+
+    @Setter
+    private String imageIdentifier;
+
+    @ManyToMany
+    private final List<Timeline> timelines = new ArrayList<>();
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Event event = (Event) o;
+        return getName().equals(event.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
 }

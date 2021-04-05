e23f6174b373297c792a5d14d40e0f45b917d9d1
Ondrej Machala <ondra097@seznam.cz>
2021-04-05 23:12:38 +0200

    Simple tests for study group dao

--- src/test/java/cz/muni/fi/pa165/dao/StudyGroupDaoTest.java
+++ src/test/java/cz/muni/fi/pa165/dao/StudyGroupDaoTest.java
@@ -1,4 +1,77 @@
 package cz.muni.fi.pa165.dao;
 
-public class StudyGroupDaoTest {
+import cz.muni.fi.pa165.PersistenceApplicationContext;
+import cz.muni.fi.pa165.entity.StudyGroup;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.InvalidDataAccessApiUsageException;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+import org.springframework.transaction.annotation.Transactional;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.validation.ConstraintViolationException;
+
+import static org.testng.Assert.*;
+
+@ContextConfiguration(classes = PersistenceApplicationContext.class)
+@TestExecutionListeners(TransactionalTestExecutionListener.class)
+@Transactional
+public class StudyGroupDaoTest extends AbstractTestNGSpringContextTests {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Autowired
+    private StudyGroupDao studyGroupDao;
+
+    @BeforeMethod
+    public void setup() {
+        StudyGroup a1 = new StudyGroup("A1");
+        StudyGroup a2 = new StudyGroup("A2");
+        StudyGroup b1 = new StudyGroup("B1");
+        StudyGroup b2 = new StudyGroup("B2");
+        em.persist(a1);
+        em.persist(a2);
+        em.persist(b1);
+        em.persist(b2);
+    }
+
+    @Test(expectedExceptions = ConstraintViolationException.class)
+    private void create_givenStudyGroupWithNullName_throw() {
+        StudyGroup studyGroup = new StudyGroup(null);
+        studyGroupDao.create(studyGroup);
+    }
+
+    @Test
+    private void create_givenValidStudyGroup_itIsPersisted() {
+        StudyGroup newStudyGroup = new StudyGroup("C1");
+        studyGroupDao.create(newStudyGroup);
+        StudyGroup studyGroup = studyGroupDao.findById(newStudyGroup.getId());
+        assertNotNull(studyGroup);
+        assertEquals(studyGroup.getName(), newStudyGroup.getName());
+    }
+
+
+    @Test
+    private void findByName_givenExistingName_returnStudyGroup() {
+        StudyGroup studyGroup = studyGroupDao.findByName("A1");
+        assertNotNull(studyGroup);
+        assertEquals(studyGroup.getName(),"A1");
+    }
+
+    @Test
+    private void findByName_givenNonExistingName_returnNull() {
+        StudyGroup studyGroup = studyGroupDao.findByName("NonexistentName");
+        assertNull(studyGroup);
+    }
+
+    @Test(expectedExceptions = InvalidDataAccessApiUsageException.class)
+    private void findByName_givenNullName_throw() {
+        StudyGroup studyGroup = studyGroupDao.findByName(null);
+    }
 }

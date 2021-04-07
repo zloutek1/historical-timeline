5bbf9b99280ae8dd700878d7066e83dc399de59c
Tomáš Ljutenko <485503@mail.muni.cz>
2021-04-07 16:52:06 +0200

    added docstrings and @Repository

--- src/main/java/cz/muni/fi/pa165/dao/UserDao.java
+++ src/main/java/cz/muni/fi/pa165/dao/UserDao.java
@@ -4,6 +4,11 @@ import cz.muni.fi.pa165.entity.User;
 
 import java.util.Optional;
 
+/**
+ * Dao interface for timeline entity
+ *
+ * @author Tomáš Ljutenko
+ */
 public interface UserDao {
     Optional<User> findByEmail(String email);
 }

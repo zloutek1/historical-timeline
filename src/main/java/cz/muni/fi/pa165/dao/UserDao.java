29821d784dfb821f6985d25ad63e1d1923096d41
David Sevcik <david@MacBook-Pro.local>
2021-04-07 21:20:49 +0200

    edited some javadoc regarding user DAO

--- src/main/java/cz/muni/fi/pa165/dao/UserDao.java
+++ src/main/java/cz/muni/fi/pa165/dao/UserDao.java
@@ -9,9 +9,9 @@ import java.util.Optional;
  */
 public interface UserDao extends CrudDao<User, Long> {
     /**
-     * If user with given email is found, returns optional object that contains it, otherwise it contains null
+     * Searches for users with given email
      * @param email email of the user
-     * @return optional container object that either contains User object or contains null value (if there is no user with given email)
+     * @return user entity if exists, empty value otherwise
      */
     Optional<User> findByEmail(String email);
 }

174702f4754450ed23becd3bae9c36ed76f276ec
Ondrej Machala <ondra097@seznam.cz>
2021-04-07 17:08:48 +0200

    Simplified studygroupdaoimpl

--- src/main/java/cz/muni/fi/pa165/dao/StudyGroupDao.java
+++ src/main/java/cz/muni/fi/pa165/dao/StudyGroupDao.java
@@ -4,6 +4,9 @@ import cz.muni.fi.pa165.entity.StudyGroup;
 
 import java.util.Optional;
 
+/**
+ * @author Ondřej Machala
+ */
 public interface StudyGroupDao extends CrudDao<StudyGroup, Long> {
     Optional<StudyGroup> findByName(String name);
 }

664bfada75b40e30736e959b48a21168b87084f3
Ondrej Machala <ondra097@seznam.cz>
2021-04-07 21:43:26 +0200

    Javadocs for study group + comment daos

--- src/main/java/cz/muni/fi/pa165/dao/CommentDao.java
+++ src/main/java/cz/muni/fi/pa165/dao/CommentDao.java
@@ -9,5 +9,11 @@ import java.util.List;
  * @author Ondřej Machala
  */
 public interface CommentDao extends CrudDao<Comment, Long> {
+    /**
+     * Finds comments belonging under the given timeline
+     *
+     * @param timeline timeline under which the comments belong
+     * @return list of all the comments under the timeline
+     */
     List<Comment> findByTimeline(Timeline timeline);
 }

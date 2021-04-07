5933a6e3ddf75f60de63006a86fd6352f21f0969
Ondrej Machala <ondra097@seznam.cz>
2021-04-07 21:43:26 +0200

    Javadocs for study group + comment daos

--- src/main/java/cz/muni/fi/pa165/dao/StudyGroupDao.java
+++ src/main/java/cz/muni/fi/pa165/dao/StudyGroupDao.java
@@ -8,5 +8,12 @@ import java.util.Optional;
  * @author Ondřej Machala
  */
 public interface StudyGroupDao extends CrudDao<StudyGroup, Long> {
+
+    /**
+     * Finds the study group with the given name
+     *
+     * @param name name of the study group
+     * @return the study group if present, else empty value
+     */
     Optional<StudyGroup> findByName(String name);
 }

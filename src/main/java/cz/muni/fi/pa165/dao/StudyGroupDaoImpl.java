87b124b8787d070aea7d60d46884cf175ccf9249
Ondrej Machala <ondra097@seznam.cz>
2021-04-07 17:08:48 +0200

    Simplified studygroupdaoimpl

--- src/main/java/cz/muni/fi/pa165/dao/StudyGroupDaoImpl.java
+++ src/main/java/cz/muni/fi/pa165/dao/StudyGroupDaoImpl.java
@@ -4,10 +4,11 @@ import cz.muni.fi.pa165.entity.StudyGroup;
 import lombok.NonNull;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.NoResultException;
-import javax.validation.constraints.NotNull;
 import java.util.Optional;
 
+/**
+ * @author Ondřej Machala
+ */
 @Repository
 public class StudyGroupDaoImpl extends CrudDaoImpl<StudyGroup, Long> implements StudyGroupDao {
 
@@ -17,13 +18,8 @@ public class StudyGroupDaoImpl extends CrudDaoImpl<StudyGroup, Long> implements 
 
     @Override
     public Optional<StudyGroup> findByName(@NonNull String name) {
-        try {
-            StudyGroup studyGroup = entityManager.createQuery("SELECT sg FROM StudyGroup sg WHERE sg.name LIKE :name", StudyGroup.class)
-                    .setParameter("name", '%' + name + '%')
-                    .getSingleResult();
-            return Optional.of(studyGroup);
-        } catch (NoResultException ex) {
-            return Optional.empty();
-        }
+        return entityManager.createQuery("SELECT sg FROM StudyGroup sg WHERE sg.name LIKE :name", StudyGroup.class)
+                .setParameter("name", '%' + name + '%')
+                .getResultStream().findFirst();
     }
 }

6dc53d8fda2d35cf4198c4e7d7cf16834321205d
Tomáš Ljutenko <485503@mail.muni.cz>
2021-04-07 22:00:34 +0200

    added docstring to findByName

--- src/main/java/cz/muni/fi/pa165/dao/TimelineDao.java
+++ src/main/java/cz/muni/fi/pa165/dao/TimelineDao.java
@@ -9,6 +9,12 @@ import java.util.Optional;
  */
 public interface TimelineDao extends CrudDao<Timeline, Long> {
 
+    /**
+     * Finds a timeline with a given name
+     *
+     * @param name name of the timeline
+     * @return timeline entity if exists, empty value otherwise
+     */
     Optional<Timeline> findByName(String name);
 
 }

0d61e9a41707d10161b10e090e3aa8d6ae266a12
Tomáš Ljutenko <485503@mail.muni.cz>
2021-04-23 16:41:11 +0200

    fixed star imports

--- persistence/src/main/java/cz/muni/fi/pa165/entity/Comment.java
+++ persistence/src/main/java/cz/muni/fi/pa165/entity/Comment.java
@@ -5,7 +5,12 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 import java.util.Objects;
--- persistence/src/main/java/cz/muni/fi/pa165/entity/User.java
+++ persistence/src/main/java/cz/muni/fi/pa165/entity/User.java
@@ -6,9 +6,20 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
 
 /**
  * Class that represents user entity

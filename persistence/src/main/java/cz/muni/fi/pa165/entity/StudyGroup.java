3cf8d3ad96aba11f1a55e9aada6b372ed2832458
zloutek1 <zloutek1@gmail.com>
2021-04-23 18:56:51 +0200

    Checkstyle (#49)
    * added checkstyle config
    
    * added checkstyle to Github actions
    
    * better configuration for checkstyle action
    
    * added ModifierOrder to check
    
    * added empty statement check
    
    * fixed star imports
    
    * added checkstyle to the build process
    
    * renamed tool_name
    
    * fixed checkstyle errors
    
    * don't run checkstyle on build
    
    * checkstyle report as a comment
    
    * inserted sample checkstyle error
    
    * changed checkstyle Github action and cleaned up steps
    
    * don't fail checkstyle on build
    
    * added verify back
    
    * added checkout back to each action
    
    * Fixed "Unable to locate Source XRef to link to - DISABLED" warning
    
    * fix sample codestyle error

--- persistence/src/main/java/cz/muni/fi/pa165/entity/StudyGroup.java
+++ persistence/src/main/java/cz/muni/fi/pa165/entity/StudyGroup.java
@@ -5,7 +5,13 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
--- persistence/src/test/java/cz/muni/fi/pa165/dao/CommentDaoTest.java
+++ persistence/src/test/java/cz/muni/fi/pa165/dao/CommentDaoTest.java
@@ -22,7 +22,9 @@ import javax.validation.ConstraintViolationException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-import static org.testng.Assert.*;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
 
 /**
  * @author David Sevcik

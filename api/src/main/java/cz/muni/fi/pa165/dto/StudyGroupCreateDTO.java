a8205fd206e5ea80eed30a726357194605cf3fd1
Ondrej Machala <ondra097@seznam.cz>
2021-04-24 18:25:11 +0200

    Move model version, introduce Data annotation

--- api/src/main/java/cz/muni/fi/pa165/dto/StudyGroupCreateDTO.java
+++ api/src/main/java/cz/muni/fi/pa165/dto/StudyGroupCreateDTO.java
@@ -1,17 +1,11 @@
 package cz.muni.fi.pa165.dto;
 
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.Data;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-@Getter
-@Setter
-@ToString
-@EqualsAndHashCode
+@Data
 public class StudyGroupCreateDTO {
     @NotNull
     @Size(min = 3, max = 50)
--- api/src/main/java/cz/muni/fi/pa165/dto/StudyGroupDTO.java
+++ api/src/main/java/cz/muni/fi/pa165/dto/StudyGroupDTO.java
@@ -1,14 +1,8 @@
 package cz.muni.fi.pa165.dto;
 
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.Data;
 
-@Getter
-@Setter
-@ToString
-@EqualsAndHashCode
+@Data
 public class StudyGroupDTO {
     private Long id;
     private String name;

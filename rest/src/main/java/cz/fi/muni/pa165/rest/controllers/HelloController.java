e83e7ce93d6533210b61d0caae421db82dc7357f
Ondrej Machala <ondra097@seznam.cz>
2021-05-07 12:12:15 +0200

    Add rest module

---
+++ rest/src/main/java/cz/fi/muni/pa165/rest/controllers/HelloController.java
@@ -0,0 +1,15 @@
+package cz.fi.muni.pa165.rest.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String hello() {
+        return "Hello there!";
+    }
+}

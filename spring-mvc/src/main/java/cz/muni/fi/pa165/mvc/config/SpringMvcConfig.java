edda652f22d3e8eace26b333310afb7f62c9bb5b
Rutzzz <ondra097@seznam.cz>
2021-05-13 20:49:14 +0200

    Spring mvc (#88)
    * Add Spring Mvc module
    
    * Fix checkstyle issues
    
    * Fix the fix of checkstyle issue

---
+++ spring-mvc/src/main/java/cz/muni/fi/pa165/mvc/config/SpringMvcConfig.java
@@ -0,0 +1,72 @@
+package cz.muni.fi.pa165.mvc.config;
+
+import cz.muni.fi.pa165.config.ServiceConfiguration;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.validation.Validator;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@EnableWebMvc
+@Configuration
+@Import({ServiceConfiguration.class})
+@ComponentScan(basePackages = "cz.muni.fi.pa165.mvc.controllers")
+public class SpringMvcConfig implements WebMvcConfigurer {
+
+    private static final String TEXTS = "Texts";
+
+    /**
+     * Maps the main page to a specific view.
+     */
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("home");
+    }
+
+
+    /**
+     * Enables default Tomcat servlet that serves static files.
+     */
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+    /**
+     * Provides mapping from view names to JSP pages in WEB-INF/jsp directory.
+     */
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/jsp/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+
+    /**
+     * Provides localized messages.
+     */
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename(TEXTS);
+        return messageSource;
+    }
+
+    /**
+     * Provides JSR-303 Validator.
+     */
+    @Bean
+    public Validator validator() {
+        return new LocalValidatorFactoryBean();
+    }
+}

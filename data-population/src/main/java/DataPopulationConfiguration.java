117d3c80e08988e9d2441bcbb7d071499873c994
Tomáš Ljutenko <485503@mail.muni.cz>
2021-05-13 23:02:09 +0200

    implemented population facade

---
+++ data-population/src/main/java/DataPopulationConfiguration.java
@@ -0,0 +1,22 @@
+import cz.muni.fi.pa165.config.ServiceConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+import javax.annotation.PostConstruct;
+
+@Configuration
+@Import(ServiceConfiguration.class)
+@ComponentScan(basePackageClasses = {PopulationFacadeImpl.class})
+public class DataPopulationConfiguration {
+
+    @Autowired
+    PopulationFacade populationFacade;
+
+    @PostConstruct
+    public void populate() {
+        populationFacade.populate();
+    }
+
+}
---
+++ data-population/src/main/java/PopulationFacade.java
@@ -0,0 +1,12 @@
+
+
+/**
+ * @author Tomáš Ljutenko
+ */
+public interface PopulationFacade {
+
+    /**
+     * Populate the database with sample data
+     */
+    void populate();
+}

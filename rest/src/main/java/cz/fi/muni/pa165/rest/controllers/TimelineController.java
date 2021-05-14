6da9ac8838077f82a3f65545297e2739ef1b5644
Eva Krajníková <xkrajnik@fi.muni.cz>
2021-05-14 20:29:56 +0200

    Changed @mapping annotations

--- rest/src/main/java/cz/fi/muni/pa165/rest/controllers/TimelineController.java
+++ rest/src/main/java/cz/fi/muni/pa165/rest/controllers/TimelineController.java
@@ -7,10 +7,13 @@ import cz.muni.fi.pa165.dto.TimelineUpdateDTO;
 import cz.muni.fi.pa165.facade.TimelineFacade;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.inject.Inject;
@@ -23,51 +26,51 @@ public class TimelineController {
     @Inject
     private TimelineFacade timelineFacade;
 
-    @RequestMapping(value = "/create", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "/create", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public final TimelineDTO createTimeline(@RequestBody TimelineCreateDTO timeline){
         Long id = timelineFacade.createTimeline(timeline);
         return timelineFacade.findById(id).orElse(null);
     }
 
-    @RequestMapping(value = "/update", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @PutMapping(value = "/update", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public final TimelineDTO updateTimeline(@RequestBody TimelineUpdateDTO timeline){
         timelineFacade.updateTimeline(timeline);
         return timelineFacade.findById(timeline.getId()).orElse(null);
     }
 
-    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @DeleteMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public final void deleteTimeline(@PathVariable Long id){
         timelineFacade.deleteTimeline(id);
     }
 
-    @RequestMapping(value = "/{timelineId}/event", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "/{timelineId}/event", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public final TimelineDTO addEvent(@PathVariable Long timelineId, @RequestBody EventDTO event){
         timelineFacade.addEvent(timelineId, event.getId());
         return timelineFacade.findById(timelineId).orElse(null);
     }
 
-    @RequestMapping(value = "/{timelineId}/event/{eventId}", method = RequestMethod.DELETE, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @DeleteMapping(value = "/{timelineId}/event/{eventId}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public final TimelineDTO removeEvent(@PathVariable Long timelineId, @PathVariable Long eventId){
         timelineFacade.removeEvent(timelineId, eventId);
         return timelineFacade.findById(timelineId).orElse(null);
     }
 
-    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public final List<TimelineDTO> findAll(){
         return timelineFacade.findAll();
     }
 
-    @RequestMapping(value = "/between/{since}/{to}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/between/{since}/{to}", produces = MediaType.APPLICATION_JSON_VALUE)
     public final List<TimelineDTO> findAllBetweenDates(@PathVariable @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate since, @PathVariable @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate to){
         return timelineFacade.findAllBetweenDates(since, to);
     }
 
-    @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public final TimelineDTO findById(@PathVariable Long id){
         return timelineFacade.findById(id).orElse(null);
     }
 
-    @RequestMapping(value = "/named/{name}",method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/named/{name}", produces = MediaType.APPLICATION_JSON_VALUE)
     public final TimelineDTO findByName(@PathVariable("name") String name){
         return timelineFacade.findByName(name).orElse(null);
     }

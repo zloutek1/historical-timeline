e62f2fd97161a6014d67f9cdc5463018ea34982c
Ondrej Machala <ondra097@seznam.cz>
2021-05-15 23:28:35 +0200

    Fix user facade + authenticate dto

--- api/src/main/java/cz/muni/fi/pa165/dto/UserAuthenticateDTO.java
+++ api/src/main/java/cz/muni/fi/pa165/dto/UserAuthenticateDTO.java
@@ -2,9 +2,7 @@ package cz.muni.fi.pa165.dto;
 
 import lombok.Data;
 
-import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 
 /**
  * @author David Sevcik
@@ -12,10 +10,8 @@ import javax.validation.constraints.Size;
 @Data
 public class UserAuthenticateDTO {
     @NotNull
-    @Email
     private String email;
 
     @NotNull
-    @Size(min = 8, max = 64)
     private String password;
 }

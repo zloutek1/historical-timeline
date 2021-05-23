8072ec176e0f3c2c0710913b219d7896b11310e4
Eva Krajníková <xkrajnik@fi.muni.cz>
2021-05-23 20:49:24 +0200

    Removed unused imports

--- spring-mvc/src/main/java/cz/muni/fi/pa165/mvc/controllers/EventController.java
+++ spring-mvc/src/main/java/cz/muni/fi/pa165/mvc/controllers/EventController.java
@@ -7,7 +7,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,11 +14,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.view.RedirectView;
 
 import javax.inject.Inject;
 import javax.validation.Valid;
-import java.io.IOException;
 
 @Controller
 @RequestMapping("/event")

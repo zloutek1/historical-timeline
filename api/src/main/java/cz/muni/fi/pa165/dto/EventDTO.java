f3581fea3cc9aba5df10cd30f3c5234c4adce9a3
Ekknd2 <42540145+Ekknd2@users.noreply.github.com>
2021-06-07 04:16:43 +0200

    Added throwing warnings (#117)
    Added EventDTO validations
    Changed dimensions

--- api/src/main/java/cz/muni/fi/pa165/dto/EventDTO.java
+++ api/src/main/java/cz/muni/fi/pa165/dto/EventDTO.java
@@ -2,6 +2,8 @@ package cz.muni.fi.pa165.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 /**
@@ -11,6 +13,8 @@ import java.time.LocalDate;
 @Data
 public class EventDTO {
     private Long id;
+    @NotNull
+    @Size(min = 3, max = 50)
     private String name;
     private LocalDate date;
     private String location;
